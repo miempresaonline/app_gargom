ed75e00c902b3374cccb811d736cfb8da4dc0505
Mi Empresa Online <dev@miempresaonline.com>
2026-04-28 19:18:23 +0200

    Revert to PM2 with disabled DirectoryIndex

--- deploy.js
+++ deploy.js
@@ -18,8 +18,8 @@ conn.on('ready', () => {
     export PATH=$NODE_DIR:$PATH &&
     echo "Usando NPM en: $NPM_BIN" &&
     npm install && 
-    npm run build &&
-    mkdir -p tmp && touch tmp/restart.txt
+    npm run build && 
+    npx pm2 start npm --name "gargom" -- run start -- -p 3005 || npx pm2 restart gargom
   `;
   
   conn.exec(cmd, (err, stream) => {
--- server.js
+++
@@ -1,3 +0,0 @@
-process.env.NODE_ENV = 'production';
-process.argv = [process.argv[0], process.argv[1], 'start', '-p', process.env.PORT || '3000'];
-require('next/dist/bin/next');
